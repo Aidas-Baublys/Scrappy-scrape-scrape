5d2177e84afb9831446303768fbd8d5510adb75a
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2022-06-16 10:18:15 +0300

    Refactor

--- scrapers/porn.py
+++ scrapers/porn.py
@@ -57,18 +57,19 @@ def initial_greetings():
     print(
         "\nAwesome! You'll see that writing wont be as hard as you when we're done.\n"
     )
-    sleep(3)
+    sleep(4)
     print(
         "First, tell me how many chapters of epic fan fiction do you want me to write?\n"
     )
     sleep(1)
     print("Quick note: I will need a word or phrase from you for every chapter.")
-    sleep(2)
-    print(
-        "So if you give 5, you will need to -- putin -- 5 words later for inspiration.\n\n"
-    )
     sleep(3)
+    print(
+        "So if you give 5, you will need to -- putin -- 5 words later for inspiration.\n"
+    )
+    sleep(4)
     print("Don't go crazy if you don't want to go all the way, honey.")
+    sleep(2)
     print("This is not Ukraine.\n")
     sleep(2)
     input("Cool? Press any key to continue.\n")
@@ -84,7 +85,6 @@ def get_int_from_user():
     got_int = False
     while not got_int:
         user_given_page = input("Whole positive number, please: ")
-        print(" ")
         try:
             page_num = int(user_given_page)
 
@@ -94,7 +94,9 @@ def get_int_from_user():
             else:
                 got_int = True
         except ValueError:
-            print(f"Whole positive numbers are 7, 13, 420, and not {user_given_page}.")
+            print(
+                f"Whole positive numbers are 7, 13, 420, and not '{user_given_page}'."
+            )
             show_try_again_msg()
         except Exception:
             print("I don't even know what you did, you naughty so and so...")
@@ -122,7 +124,7 @@ def scrape_and_write():
     while current_page <= last_page:
         if current_page == 1:
             inspire_words()
-            search = input("Ready? Your word: \n").capitalize()
+            search = input("Ready? Your word: \n")
         elif current_page == last_page:
             search = input("And the last one: \n")
         else:
@@ -130,7 +132,9 @@ def scrape_and_write():
 
         porn_headlines = get_porn_headlines(current_url)
         filtered_porn_headlines = filter_porn_headlines(search, porn_headlines)
-        make_50_shades_manuscript(filtered_porn_headlines, current_page, search)
+        make_50_shades_manuscript(
+            filtered_porn_headlines, current_page, search.capitalize()
+        )
 
         porn_soup = make_soup(current_url)
         all_links = porn_soup.find_all("a", href=True)
