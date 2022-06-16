723709c9a17cc4fe5561acf5360a120c26034036
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2022-06-16 19:01:08 +0300

    Add auto launch script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/Aidas-Baublys/Scrappy-scrape-scrape.git
+cd Scrappy-scrape-scrape/
+pip install --upgrade pipenv --user
+pipenv install
+pipenv shell
+cd ..
+pipenv run python ./Scrappy-scrape-scrape/main.py
