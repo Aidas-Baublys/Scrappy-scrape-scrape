e787eacc81192f44d6a00c75720dbd9c00b082ab
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2022-06-17 09:58:07 +0300

    Update launch script

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@ git clone https://github.com/Aidas-Baublys/Scrappy-scrape-scrape.git
 cd Scrappy-scrape-scrape/
 py -m pip install --upgrade pipenv --user
 py -m pipenv install
+cd Scrappy-scrape-scrape/
 py -m pipenv shell
 py main.py
